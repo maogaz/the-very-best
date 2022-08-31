f7166f37a4f7ef4b0010d197a291dcf2ad36256e
maogaz <maogaz@uc.cl>
2022-08-30 22:03:48 -0400

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+//import '../stylesheets/login.css'
+import '../stylesheets/NavBar.css'
+
+function NavBar(){
+    return (
+        <div>
+            <nav>
+                <a href="#">Música</a>
+                <a href="#">Películas</a>
+                <a href="#">Series</a>
+                <a href="#">Libros</a>
+            </nav>
+        </div>
+    );    
+}
+
+export default NavBar;
